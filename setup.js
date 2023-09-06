54c216adab9493773f426b2728b2667d8a7b0efa
Bastian Hundt <bhundt@users.noreply.github.com>
2023-09-06 21:19:40 +0200

    Initial commit

---
+++ setup.js
@@ -0,0 +1,15 @@
+export const player = {
+  run1: "/assets/fw_1.png",
+  run2: "/assets/gw_2.png",
+  jumping: "/assets/sj.png",
+  lose: "/assets/fh.png",
+  rotate: true
+};
+
+//export const theme = "bw";
+export const customColors = {
+  textColor: "midnightblue",
+  textBackgroundColor: "papayawhip",
+  skyColor: "lightsalmon",
+  gameBackgroundColor: "midnightblue"
+};
